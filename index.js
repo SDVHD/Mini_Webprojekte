982ba8bd80185551649632134f242279f516602e
SDVHD <swissdavinci@gmail.com>
2020-05-31 01:03:55 +0200

    Add Switch chose_operation

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ var num1,
 function click_enter() {
     document.getElementById("div_output").innerText = ergebnis;
 }
+
+function chose_operation() {
+    switch (operation) {
+       case 0: 
+            operation = "+";
+            break;
+        case 1:
+            operation = "-";
+            break;
+        case 2:
+            operation = "/"
+            break;
+        case 3: 
+            operation = "*"
+            break;   
+    }
+}
