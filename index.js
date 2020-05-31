d7dc7098a2193f760b1a31a9289bd8939a5f2dae
SDVHD <swissdavinci@gmail.com>
2020-05-31 20:29:12 +0200

    Add big Update

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-var num1, 
-    num2,  
-    ergebnis;
-
-
-function click_enter() {
-    document.getElementById("div_output").innerText = ergebnis;
-}
-
-function chose_operation(withbutton) {
-    switch (withbutton) {
-       case 0: 
-            operation = "+";
-            console.log(operation);
-            break;
-        case 1:
-            operation = "-";
-            console.log(operation);
-            break;
-        case 2:
-            operation = "/"
-            console.log(operation);
-            break;
-        case 3: 
-            operation = "*"
-            console.log(operation);
-            break;   
-    }
+function chose(val) {
+    document.getElementById("div_output").value+=val;
 }
