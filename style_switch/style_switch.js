a50965716057bf403f5cf8d1e9dad88d399602a5
SDVHD <swissdavinci@gmail.com>
2020-06-01 00:28:13 +0200

    Add Rainbow Styel

--- style_switch/style_switch.js
+++ style_switch/style_switch.js
@@ -5,21 +5,21 @@ function style_classic() {
 function style_rainbow() {
     document.getElementById("h1_titel").className = "h1_rainbow";
     document.getElementById("in_output").className = "in_output_rainbow";
-    document.getElementById("btn_multiplikation").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_division").style.backgroundColor = "#53C7CB";
-    document.getElementById("btn_substraktion").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_addition").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_7").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_8").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_9").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_4").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_5").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_6").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_1").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_2").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_3").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_0").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_enter").style.backgroundColor = "#B8D331";
-    document.getElementById("btn_reset").style.backgroundColor = "#B8D331";
+    document.getElementById("btn_multiplikation").className = "btn_rainbow_green";
+    document.getElementById("btn_division").className = "btn_rainbow_blue";
+    document.getElementById("btn_substraktion").className = "btn_rainbow_yellow";
+    document.getElementById("btn_addition").className = "btn_rainbow_darkred";
+    document.getElementById("btn_7").className = "btn_rainbow_darkred";
+    document.getElementById("btn_8").className = "btn_rainbow_red";
+    document.getElementById("btn_9").className = "btn_rainbow_green";
+    document.getElementById("btn_4").className = "btn_rainbow_yellow";
+    document.getElementById("btn_5").className = "btn_rainbow_blue";
+    document.getElementById("btn_6").className = "btn_rainbow_darkred";
+    document.getElementById("btn_1").className = "btn_rainbow_red";
+    document.getElementById("btn_2").className = "btn_rainbow_green";
+    document.getElementById("btn_3").className = "btn_rainbow_yellow";
+    document.getElementById("btn_0").className = "btn_rainbow_blue";
+    document.getElementById("btn_enter").className = "btn_rainbow_red";
+    document.getElementById("btn_reset").className = "btn_rainbow_yellow";
 
 }
