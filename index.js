3bcde8679e63caa7763304466afd075e061c8006
SDVHD <swissdavinci@gmail.com>
2020-05-31 20:49:35 +0200

    Calc fertig

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+
 function chose(val) {
     document.getElementById("div_output").value+=val;
+}
+
+function enter() {
+    let zahl1 = document.getElementById("div_output").value;
+    let ergebnis = eval(zahl1);
+    document.getElementById("div_output").value = ergebnis;
+}
+
+function clear() {
+    document.getElementById("div_output").value = "00";
 }
